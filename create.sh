8532884c176c24ae46cef13e8a5297ef3633e312
apple <apple@applehq.eu>
2012-03-15 00:54:41 +0000

    Updates

--- create.sh
+++ create.sh
@@ -13,15 +13,16 @@ copy() {
 	hl=$1
 	il=$2
 
-	test "$(ldd $hl)" = "not a dynamic executable" && cp -L $hl $(dirname $0)/$il
+	test "$(ldd $hl)" = "	not a dynamic executable" && cp -L $hl $(dirname $0)/$il
 }
 
 newdirs "$(dirname $0)" bin sbin dev proc sys newroot etc root lib
 
 copy /bin/busybox.static bin/busybox
 copy /sbin/cryptsetup sbin
-copy /sbin/lvm.static sbin/lvm
+copy /sbin/lvm.static sbin/lvm && chmod +w sbin/lvm
 
 ln -sf ../bin/busybox $(dirname $0)/sbin/mdev
 
-find $(dirname $0) -print0 | cpio --null -ov --format=newc | gzip -9 > ${1:-rd.cpio.gz}
+find $(dirname $0) -name .git -prune -o \( -print0 \) | cpio --null -ov --format=newc | gzip -9 > ${1:-/tmp/rd.cpio.gz}
+
